64a4140a767abd7bb81fe7fef22f2aae0481ab35
NullableInt <42067409+NullableInt@users.noreply.github.com>
2018-08-31 14:57:54 +0200

    Updated index. Added _ShowCharacters partial that crashes.

---
+++ src/dndChar.Api/Pages/Shared/_ShowCharacters.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using dndChar;
+using dndChar.Database;
+
+namespace dndChar.Api.Pages.Shared
+{
+    public class _ShowCharactersModel : PageModel
+    {
+        private readonly dndChar.Database.ApplicationDbContext _context;
+
+        public _ShowCharactersModel(dndChar.Database.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CharacterSheet> CharacterSheet { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            CharacterSheet = await _context.Characters.ToListAsync();
+        }
+    }
+}
