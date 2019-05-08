f3803b19271389cfd5f54a9d562d8906a258a684
Nullint <granerud.daniel@gmail.com>
2019-05-08 16:52:41 +0200

    Fix PascalCasing on models

--- src/dndCharApi/Models/RpgChar/RpgCharModel.cs
+++ src/dndCharApi/Models/RpgChar/RpgCharModel.cs
@@ -9,45 +9,25 @@ namespace dndCharApi.Models.RpgChar
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        [BsonElement("ownerID")]
         public string OwnerID { get; set; }
-        [BsonElement("profile")]
         public Profile Profile { get; set; }
-        [BsonElement("traits")]
         public List<Trait> Traits { get; set; }
-        [BsonElement("items")]
         public List<Item> Items { get; set; }
-        [BsonElement("abilityScores")]
         public AbilityScores AbilityScores { get; set; }
-        [BsonElement("status")]
         public List<Status> Status { get; set; }
-        [BsonElement("hitDice")]
         public List<HitDice> HitDice { get; set; }
-        [BsonElement("health")]
         public Health Health { get; set; }
-        [BsonElement("savingThrows")]
         public List<SavingThrow> SavingThrows { get; set; }
-        [BsonElement("skills")]
         public List<Skill> Skills { get; set; }
-        [BsonElement("hitDiceType")]
         public List<HitDiceTypeModel> HitDiceType { get; set; }
-        [BsonElement("deathSave")]
         public List<DeathSave> DeathSave { get; set; }
-        [BsonElement("treasure")]
         public List<Treasure> Treasure { get; set; }
-        [BsonElement("characterAppearance")]
         public List<CharacterAppearance> CharacterAppearance { get; set; }
-        [BsonElement("featuresTraits")]
         public List<FeaturesTrait> FeaturesTraits { get; set; }
-        [BsonElement("equipment")]
         public Equipment Equipment { get; set; }
-        [BsonElement("magicItems")]
         public List<MagicItem> MagicItems { get; set; }
-        [BsonElement("notes")]
         public List<Note> Notes { get; set; }
-        [BsonElement("spells")]
         public Spells Spells { get; set; }
-        [BsonElement("feats")]
         public List<Feat> Feats { get; set; }
         public BsonDateTime _created { get; set; }
         public BsonDateTime _lastUpdated { get; set; }
