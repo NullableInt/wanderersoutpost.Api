2e96fce93a4c7d8dad9d61525ca98cee6885dde9
Daniel Granerud <daniel.granerud@payex.com>
2020-02-28 11:26:27 +0100

    Docker works?

--- src/TheWanderersOutpost.Api/Program.cs
+++ src/TheWanderersOutpost.Api/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System.IO;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Sentry;
 
 namespace TheWanderersOutpost.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -34,29 +32,8 @@ namespace TheWanderersOutpost.Api
                         logging.AddConsole();
                         logging.AddDebug();
                         logging.AddEventSourceLogger();
-                    }).UseStartup<Startup>().UseSentry();
+                    }).UseStartup<Startup>();
                 });
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddCommandLine(args)
-                .Build();
-
-            return WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5000")
-                .UseConfiguration(config)
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    logging.AddConsole();
-                    logging.AddDebug();
-                    logging.AddEventSourceLogger();
-                })
-                .UseStartup<Startup>()
-                .UseSentry();
         }
     }
 }
