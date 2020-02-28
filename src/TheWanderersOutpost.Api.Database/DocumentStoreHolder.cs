438c94b49ab2806df5b13bc35eb9b9f3616d4912
Daniel Granerud <daniel.granerud@payex.com>
2020-02-28 13:06:04 +0100

    wsfdgthn

--- src/TheWanderersOutpost.Api.Database/DocumentStoreHolder.cs
+++ src/TheWanderersOutpost.Api.Database/DocumentStoreHolder.cs
@@ -2,16 +2,16 @@
 
 namespace TheWanderersOutpost.Api.Database
 {
-    public class DocumentStoreHolder
+  public class DocumentStoreHolder
+  {
+    public const string CharDatabaseName = "RpgCharModelDb";
+    public DocumentStoreHolder()
     {
-        public const string CharDatabaseName = "RpgCharModelDb";
-        public DocumentStoreHolder()
-        {
-            Store = new MongoClient(System.Environment.GetEnvironmentVariable("MongodbUrl"));
-        }
-
-        public IMongoClient Store { get; private set; }
-
-        public IMongoDatabase GetDefaultDatabase() => Store.GetDatabase(CharDatabaseName);
+      Store = new MongoClient(System.Environment.GetEnvironmentVariable("MongodbUrl"));
     }
+
+    public IMongoClient Store { get; private set; }
+
+    public IMongoDatabase GetDefaultDatabase() => Store.GetDatabase(CharDatabaseName);
+  }
 }
